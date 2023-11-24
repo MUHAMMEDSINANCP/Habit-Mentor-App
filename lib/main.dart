b5d5e8e6dc8c6dd46ef6c67ed1f3d40231be5d30
Muhammed Sinan CP <cpmuhammedsinan@gmail.com>
2023-11-25 02:50:35 +0530

    Successfully Completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit Mentor',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.from(
           colorScheme:
